54df1bf26339e6105e2871d1de803d0422a48c79
Maken Hubbard <makhub26@outlook.com>
2018-09-11 17:06:15 -0500

    Add project files.

---
+++ FamilyDict/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FamilyDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
